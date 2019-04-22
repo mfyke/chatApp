71ffe77067def687cb8a395c9a5c3f55e111da2b
matt fyke <mfyke@live.com>
2019-04-22 15:41:49 -0700

    basic chat able to send with specified handle and message

--- index.js
+++ index.js
@@ -21,4 +21,8 @@ const io = socket(server);
 
 io.on('connection', (socket)=>{
     console.log('made socket connection', socket.id);
+
+    socket.on('chat', (data)=>{
+        io.sockets.emit('chat', data);
+    });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,17 @@
 //make connection on the client side
 const socket = io.connect('http://localhost:8000');
+
+//click events
+
+$("#send").on("click", ()=>{
+    socket.emit("chat", {
+        message: $("#message").val(),
+        handle: $("#handle").val()
+    })
+});
+
+//listen for events
+
+socket.on("chat", (data)=>{
+    $("#output").append(`<p><strong>${data.handle}:</strong>${data.message}</p>`);
+});
