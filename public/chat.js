984dc8e53e68006a729e3db642b255ed0338ace2
matt fyke <mfyke@live.com>
2019-04-23 08:52:47 -0700

    modify handle to remain after sending a message and ability to press enter as well as click send

--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,30 @@
 //make connection on the client side
 const socket = io.connect('http://localhost:8000');
 
-//click events
+//message function
 
-$("#send").on("click", ()=>{
+const cMessage = ()=>{
     socket.emit("chat", {
         message: $("#message").val(),
         handle: $("#handle").val()
-    })
+    });
+    $("#message").val("");
+    // $("#handle").val("");
+}
+
+//click events
+$("#send").on("click", ()=>{
+    cMessage();
 });
+
+//press enter
+$(document).on("keyup", (event)=>{
+    if(event.which == 13) {
+        cMessage();
+    }
+}); 
+
+
 
 //listen for events
 
